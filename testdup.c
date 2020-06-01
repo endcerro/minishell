f12c36bf6db3d0f7ddc00aad87bebef38dff4551
hpottier <hpottier@student.42.fr>
2020-06-01 12:06:36 +0200

    test dup2

---
+++ testdup.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern char	**environ;
+
+int			main(void)
+{
+	int		pipefd[2];
+	char	**argv;
+
+	pipe(pipefd);
+	if (fork() == 0)
+	{
+		argv = (char **)malloc(sizeof(char *) * 2);
+		argv[0] = strdup("ls");
+		argv[1] = strdup("-la");
+		dup2(pipefd[1], 1);
+		execve("/bin/ls", argv, environ);
+	}
+	if (fork() == 0)
+	{
+		argv = (char **)malloc(sizeof(char *) * 2);
+		argv[0] = strdup("grep");
+		argv[1] = strdup("git");
+		dup2(pipefd[0], 0);
+		execve("/bin/grep", argv, environ);
+	}
+	return (0);
+}
